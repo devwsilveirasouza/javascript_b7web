555ecec9bc7f792553b71c8c0e0defbe0dc9c03d
devwsilveirasouza <dev.wsilveirasouza@gmail.com>
2022-10-25 20:19:23 -0300

    Função com retorno condicional

--- Modulo_02/aula01.js
+++ Modulo_02/aula01.js
@@ -1,12 +1,35 @@
-/** 
- * Módulo 02 
- * 
- * Aula 01 - Funções 
- * */
+ // Módulo 02 
+  
+ // Aula 01 - Funções 
+ 
 // Criando a função
-function gravidade() {
-    console.log('A gravidade é: ');
-    console.log(9.8);
-}
+// function gravidade() {
+//     console.log('A gravidade do planeta é: ');
+//     console.log(9.8);
+// }
+
+// Aula 02 
+
 // Chamando a função
 // gravidade();
+
+// Aula 03
+
+// Passando parâmetros em funções
+
+// function somar(n1, n2) {
+//     let resultado = n1 + n2;
+//     console.log('Resultado: ' + resultado);
+// }
+
+// somar(10, 15);
+
+function nomeCompleto(nome, sobrenome) {
+    // console.log(`${nome} ${sobrenome}`);
+    return `${nome} ${sobrenome}`;
+}
+/** Retorno da função aula 05 */
+// nomeCompleto('Wellington', 'de Souza');
+let completo = nomeCompleto('Wellington', 'de Souza');
+console.log('Completo: ' + completo);
+
---
+++ Modulo_02/aula06.js
@@ -0,0 +1,18 @@
+/** Função com retorno condicional */
+function maiorDeIdade(idade) {
+    if (idade >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let idade = 19
+let verificacao = maiorDeIdade(idade);
+
+if(verificacao) {
+    console.log('É maior de idade.');
+} else {
+    console.log('É menor de idade.');
+}
+
